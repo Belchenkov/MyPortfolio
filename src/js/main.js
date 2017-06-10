badde1b5cac1b9e3b965ce617302aec890fa4123
Aleksey <u608110@gmai.com>
2017-06-10 14:24:48 +0300

    3d Animated Meni

--- src/js/main.js
+++ src/js/main.js
@@ -4,8 +4,25 @@
 //= ../../bower_components/jquery/dist/jquery.min.js
 //= ../../bower_components/tether/dist/js/tether.min.js
 //= ../../bower_components/bootstrap/dist/js/bootstrap.min.js
+//= ../../bower_components/3d-rotating-navigation/js/main.js
 
 /*
     Custom
  */
 //= partials/helper.js
+
+
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+ga('create', 'UA-48014931-1', 'codyhouse.co');
+ga('send', 'pageview');
+
+jQuery(document).ready(function($){
+    $('.close-carbon-adv').on('click', function(event){
+        event.preventDefault();
+        $('#carbonads-container').hide();
+    });
+});
