c52cd3b559eb7c85c3b6ad8ea4e05570785c58e5
Aleksey <u608110@gmai.com>
2017-06-25 16:59:55 +0300

    Works Buttons

--- src/js/main.js
+++ src/js/main.js
@@ -25,6 +25,17 @@ jQuery(document).ready(function($){
         event.preventDefault();
         $('#carbonads-container').hide();
     });
+
+    // CSS 3D Flip Box
+    //  https://codepen.io/syedrafeeq/pen/eCkFt
+    // set up hover panels
+		// although this can be done without JavaScript, we've attached these events
+		// because it causes the hover to be triggered when the element is tapped on a touch device
+		$('.hover').hover(function(){
+			$(this).addClass('flip');
+		},function(){
+			$(this).removeClass('flip');
+		});
     
 });
 
