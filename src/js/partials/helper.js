680882f7855f22158b1f68478804de81bcea16a4
Aleksey <u608110@gmai.com>
2017-07-04 21:14:10 +0300

    Scroll to block Works

--- src/js/partials/helper.js
+++ src/js/partials/helper.js
@@ -1,4 +1,13 @@
-//Так приятней :)
-window.log = function(param){
-    console.log(param);
-};
+$(document).ready(function(){
+    
+    // Плавный скролл
+    //http://dontforget.pro/javascript/plavnyiy-skrolling-k-elementu-na-jquery/
+
+    $('.go_to').click( function(){ // ловим клик по ссылке с классом go_to
+	var scroll_el = $(this).attr('href'); // возьмем содержимое атрибута href, должен быть селектором, т.е. например начинаться с # или .
+        if ($(scroll_el).length != 0) { // проверим существование элемента чтобы избежать ошибки
+	    $('html, body').animate({ scrollTop: $(scroll_el).offset().top }, 1000); // анимируем скроолинг к элементу scroll_el
+        }
+	    return false; // выключаем стандартное действие
+    });
+});
